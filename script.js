b1bc924851d8da65598c53eda5d45a6c9141d57b
Rodrigo Weller Zarrabal <cs.rgzarrabal@gmail.com>
2022-05-13 16:25:02 -0500

    Added js code to manage the count

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-window.onload = () => {console.log('hello');};
+const counter = document.querySelector('.counter');
+const decrease = document.querySelector('.counter-controls .decrease');
+const increase = document.querySelector('.counter-controls .increase');
+const reset = document.querySelector('.counter-controls .reset');
+
+let count = 0;
+
+decrease.addEventListener('click', () => {
+    count = decreaseCount(count);
+    counter.innerHTML = count.toString();
+    counter.style.color = setColor(count);
+    console.log(count);
+});
+increase.addEventListener('click', () => {
+    count = increaseCount(count);
+    counter.innerHTML = count.toString();
+    counter.style.color = setColor(count);
+    console.log(count);
+});
+reset.addEventListener('click', () => {
+    count = resetCount(count);
+    counter.innerHTML = count.toString();
+    counter.style.color = setColor(count);
+    console.log(count);
+});
+
+function increaseCount(current) {return current + 1}
+function decreaseCount(current) {return current - 1}
+function resetCount() {return 0}
+function setColor(current) { 
+    return (current > 0) ? "#30bf64" : ((current < 0) ? "#ff3d3d" : "#000000");
+}
